ba147a52f6bf4feb946a773a37e8ed6fd294d473
Shailandra <rathore.shanu1996@gmail.com>
2026-05-20 09:15:01 +0000

    conversion

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,37 @@
+let value =3
+console.log(value)
+
+let negValue = -value
+console.log(negValue)
+
+console.log(2+3)    //5
+console.log(3-2)    //1
+console.log(5/2)    //2.5
+console.log(5%2)    //1
+console.log(5*2)    //10
+console.log(5**2)   //25
+
+let str1= "Shaialandra"
+let str2 = " Rathore"
+console.log(str1+str2)   //Shaialandra Rathore
+
+console.log("1"+2)    //12
+console.log(1+"2")    //12
+console.log("1"+"2")  //12
+
+console.log("1"+2+2)  //122
+console.log(1+2+"2")  //32
+console.log("1"+2+2+3) //1223
+console.log(1+2+2+"2") //52
+
+console.log(+true)
+
+let num1, num2, num3 
+
+num1 = num2 = num3 = 2+2
+
+console.log(num1,num2,num3)
+
+let gameCounter =100
+console.log(gameCounter++)
+console.log(gameCounter)
