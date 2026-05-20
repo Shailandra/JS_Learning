775160be7bf0291311a444126e636cd7f2eb2bfe
Shailandra <rathore.shanu1996@gmail.com>
2026-05-20 08:02:20 +0000

    adding file

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // trat all js code as newer version
+
+//alert("Hello") //we are using node JS not browser
+
+console.log("Hello")
+
+let name = "Shailandra"
+let age = 23
+let isLoggedIn = true
+
+console.log(typeof(name))
+console.log(typeof age)
+
+
+// DataType (Premitive Data Type)
+// Number    -->
+// BigInt    -->
+// String    -->
+// boolean   -->true/false
+// null      -->Standalone Value
+// None      -->
+// undefined -->varible declared but not assigned any value
+// symbol    -->
+
+// Object
+console.log(typeof null)
+console.log(typeof undefined)
