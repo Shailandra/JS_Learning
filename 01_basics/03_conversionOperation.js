f633603ff8c83766d5732f85d5b0c4383aef9067
Shailandra <rathore.shanu1996@gmail.com>
2026-05-20 08:15:05 +0000

    Datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = false
+
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)   // output : NaN
+
+
+// "33" => 33
+// "33abc" => Nan
+// true  => 1
+// false => 0
+
+
+let isLoggedIn = ""
+console.log(isLoggedIn)
+
+let booleanInValue = Boolean(isLoggedIn)
+console.log(booleanInValue)
+
+// 1 ==> true  ,  0=> false
+// "" => false
+// "Shailandra"  => false
+
+
+let someNumber = 33
+console.log(someNumber)
+
+let valueInString = String(someNumber)
+console.log(valueInNumber)
