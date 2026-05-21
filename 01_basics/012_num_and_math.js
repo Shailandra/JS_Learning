b86bff58a7b4257905b1abefc06fb89f1577d98e
Shailandra <rathore.shanu1996@gmail.com>
2026-05-21 09:49:47 +0000

    Math funtion

---
+++ 01_basics/012_num_and_math.js
@@ -0,0 +1,35 @@
+// const score = 400
+// console.log(score)
+// const balanace = new Number(500)
+// console.log(balanace)
+
+// console.log(balanace.toString().length)
+// console.log(balanace)
+
+// console.log(balanace.toFixed(2));
+
+// const otherNumber = 123.543536
+// console.log(otherNumber.toPrecision(3))
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+
+
+//**********************************************Maths**********************************************
+
+// console.log(Math.PI.toPrecision(3))
+// console.log(Math.abs(-1))
+// console.log(Math.round(4.6))
+// console.log(Math.round(4.4))
+// console.log(Math.sqrt(3))
+// console.log(Math.pow(2,3))
+// console.log(Math.min(4,5,9,1))
+// console.log(Math.max(4,5,9,1))
+console.log(Math.random())
+console.log(((Math.random()*10)+1))
+console.log(Math.floor(((Math.random()*10)+1)))
+const min = 10
+const max = 20
+
+console.log((Math.flor((Math.random()*(max-min+1))+min)))
