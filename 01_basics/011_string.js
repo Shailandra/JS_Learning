ed0a8eaff645986e42d55fba5e19b02bd942d234
Shailandra <rathore.shanu1996@gmail.com>
2026-05-21 08:30:18 +0000

    String operation

--- 01_basics/011_string.js
+++ 01_basics/011_string.js
@@ -5,5 +5,10 @@ console.log(s.length)
 console.log(s.trimStart())
 console.log(s.trimEnd())
 console.log(s.includes('dra'))
-
-
+console.log(s.charAt(12))
+console.log(s.indexOf("an"))
+console.log(s.concat("Rathore"))
+console.log(s)  // concatination will not modify the string
+console.log(s.replace('a','b'))  // it will replace only first matching character
+console.log(s.replaceAll('a','b')) /// it will replace  all matching character
+console.log(s.split('a'))
