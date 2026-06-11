140f39b167fe87cce9cc7d9ab234cd074a446842
Shailandra <rathore.shanu1996@gmail.com>
2026-06-11 12:12:04 +0000

    Dom menipulation

--- 09_advance_one/Excersize9.js
+++ 09_advance_one/Excersize9.js
@@ -10,3 +10,37 @@ Fetch:
 https://jsonplaceholder.typicode.com/posts/{id}
 
 Display the title on the page. */
+
+//   <script>
+//         document.getElementById("searchBtn").addEventListener("click", () => {
+//             const id = document.getElementById("postId").value;
+
+//             fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+//                 .then(response => {
+//                     if (!response.ok) {
+//                         throw new Error("Post not found");
+//                     }
+//                     return response.json();
+//                 })
+//                 .then(data => {
+//                     document.getElementById("result").textContent = data.title;
+//                 })
+//                 .catch(error => {
+//                     document.getElementById("result").textContent = error.message;
+//                 });
+//         });
+//     </script>
+
+document.getElementById('searchBtn').addEventListener('click', () => {
+    let id = document.getElementById("postId").textContent();
+    id = parseInt(id);
+
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+        .then(response => response.json)
+        .then(data => {
+            document.getElementById('result').textContent = data.title;
+        }).catch(error => {
+            document.getElementById("result").textContent = error.message;
+        })
+}
+)
